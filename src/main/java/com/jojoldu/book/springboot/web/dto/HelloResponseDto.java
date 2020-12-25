20ca74fe61113ff7b03fde283da4e98096560f69
KIMSUHWAN <suwhean@naver.com>
2020-12-25 13:30:18 +0900

    3장 시작전 commit

---
+++ src/main/java/com/jojoldu/book/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+
+}
